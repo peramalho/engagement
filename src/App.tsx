9e7d2c6d77bf8fd57f62bb9709e711c8a95b7c61
peramalho <pedrorfig27@gmail.com>
2025-01-20 16:57:26 -0300

    Add generateInitialCards function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-type Node = {
-  name: string;
+import { useState } from "react";
+
+type Card = {
+  type: string;
   status: "hidden" | "selected" | "completed";
 };
 
-const entities = [
+const itemTypes = [
   "bullet-bill",
   "coin",
   "flower-fire-2",
@@ -19,13 +21,34 @@ const entities = [
   "yoshis-egg",
 ];
 
+// Fisher-Yates Shuffle algorithm
+const shuffleArray = <T,>(array: T[]): T[] => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const generateInitialCards = (itemTypes: string[]): Card[] => {
+  // Each item type is duplicated so they can match each other
+  const cards: Card[] = itemTypes.reduce((acc, item) => {
+    const newItem: Card = { type: item, status: "hidden" };
+    return [...acc, newItem, newItem];
+  }, [] as Card[]);
+
+  return shuffleArray(cards);
+};
+
 function App() {
+  const [cards, setCards] = useState(generateInitialCards(itemTypes));
+
   return (
     <div className="flex justify-center">
-      <div className="w-[800px]">
-        {entities.map((item) => (
-          <div key={item}>
-            <img src={`/${item}.png`} width={60} />
+      <div className="w-[800px] flex gap-4 flex-wrap">
+        {cards.map((item, index) => (
+          <div key={index} className="bg-neutral-300">
+            <img src={`/${item.type}.png`} width={60} />
           </div>
         ))}
       </div>
