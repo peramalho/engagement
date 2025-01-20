c88383405d0ec4631d525a114d62ff377a8ca0cf
peramalho <pedrorfig27@gmail.com>
2025-01-20 15:48:50 -0300

    Add icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,33 @@ type Node = {
   status: "hidden" | "selected" | "completed";
 };
 
-const cards = ["mushroom"];
+const entities = [
+  "bullet-bill",
+  "coin",
+  "flower-fire-2",
+  "flower-fire",
+  "goomba",
+  "paper-bowser",
+  "paper-mario",
+  "question",
+  "shyguy",
+  "star",
+  "super-mushroom-2",
+  "super-mushroom",
+  "yoshis-egg",
+];
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex justify-center">
+      <div className="w-[800px]">
+        {entities.map((item) => (
+          <div key={item}>
+            <img src={`/${item}.png`} width={60} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
